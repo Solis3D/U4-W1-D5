a2b79c077860e668b3004e6ffb73c89955d8193b
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-03-13 12:57:38 +0100

    Update Main to test Classes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import entities.Audio;
 import entities.Immagine;
+import entities.Video;
 
 public class Main {
     public static void main(String[] args) {
         Audio audio1 = new Audio("We Will Rock you", 5,1);
         Immagine immagine1 = new Immagine("L'infinito",0);
+        Video video1 = new Video("EpiVideo", 4,3,3);
 
         audio1.play();
+        immagine1.show();
+        video1.play();
 
 
     }
